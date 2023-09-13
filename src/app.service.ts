b745744d26a39a6ce9b6d668c5e8bc6abe8b2c5b
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-09-13 21:11:34 +0100

    feat: test deploy to dev[3]

--- src/app.service.ts
+++ src/app.service.ts
@@ -19,7 +19,7 @@ export class AppService {
 
   constructor(private http: HttpService) { }
   getHello(): string {
-    return 'Hello My Furex! ';
+    return 'Welcome Furexians!';
   }
 
   async fetchCountries() {
