d96f4a137fa74520928a27eb1104551de7cfa798
octacondeveloper <kaakadev@gmail.com>
2023-12-01 14:15:11 +0100

    revert update

--- src/middleware/whitelist.middleware.ts
+++ src/middleware/whitelist.middleware.ts
@@ -10,35 +10,17 @@ export class WhitelistMiddleware implements NestMiddleware {
     const allowedIp = process.env.WHITELISTED_IPS;
     console.log("enb-ip", process.env.WHITELISTED_IPS);
 
-    // const { ip, method, originalUrl } = request;
+    const { ip, method, originalUrl } = request;
 
-    // console.log('ip', ip);
-    // console.log('lp', Lp);
-    try{
-    fetch('https://json.geoiplookup.io/')
-      .then(data => data.json())
-      .then(data => {
-        let ip = data.ip ?? data.hostname
-         console.log('ip', ip);
-        if (allowedIp.includes(ip)) {
-          console.log("true");
-          next();
-        } else {
-          console.log("false");
-          throw new BadRequestException('You can not access this request at this moment, try again later.');
-        }
-      });
-    }catch(err)
-    {
-      throw new BadRequestException('You can not access this request at this moment, try again later.');
+    console.log('ip', ip);
+
+    if (allowedIp.includes(ip)) {
+       console.log("true");
+      next();
+    } else {
+      console.log("false");
+    throw new BadRequestException('You can not access this request at this moment, try again later.');
     }
-    // if (allowedIp.includes(ip)) {
-    //    console.log("true");
-    //   next();
-    // } else {
-    //   console.log("false");
-    // throw new BadRequestException('You can not access this request at this moment, try again later.');
-    // }
 
   }
 
