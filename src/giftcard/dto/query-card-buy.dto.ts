4f95a00316e1b0159ac65f80b977e61048e3b352
olawuyi rilwan <olawuyirilwan98@gmail.com>
2023-12-12 14:42:11 +0100

    buy transaction dto

--- src/giftcard/dto/query-card-buy.dto.ts
+++ src/giftcard/dto/query-card-buy.dto.ts
@@ -1,9 +1,78 @@
-export type QueryCardBuyDto = {
-  status?: 'PENDING' | 'CONFIRMED' | 'FAILED';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
+import { KycLevel } from 'src/utils/constants';
+
+
+export enum BuyTransactionType {
+  CONFIRMED = 'CONFIRMED',
+  PENDING = 'PENDING',
+  FAILED = 'FAILED',
+}
+/**
+ * A DTO representing data to be sent when getting users.
+ */
+export class QueryCardBuyDto {
+  @ApiPropertyOptional({ default: 40 })
+  @IsOptional()
   limit?: number;
+
+  @ApiPropertyOptional({ default: 0 })
+  @IsOptional()
   page?: number;
-  userId?: string;
-  amount?: number;
-  from?: Date;
-  to?: Date;
-};
+
+  @ApiPropertyOptional({
+    description: 'furex id',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  // @Matches(/^[a-zA-Z ]*$/, { message: 'lastName must be alphabets' })
+  userId: string;
+
+  @ApiPropertyOptional({ enum: BuyTransactionType, required: false })
+  @IsEnum(BuyTransactionType)
+  @IsOptional()
+  status: BuyTransactionType;
+
+  @ApiPropertyOptional({ description: 'user account number', required: false })
+  @IsString()
+  @IsOptional()
+  date: string;
+
+  @ApiPropertyOptional({ default: 'oldest and newest', required: false })
+  @IsOptional()
+  sortBy: string;
+
+  @ApiPropertyOptional({
+    description: 'cardName',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  // @Matches(/^[a-zA-Z ]*$/, { message: 'lastName must be alphabets' })
+  cardName: string;
+
+  @ApiPropertyOptional({
+    description: 'cardName',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  // @Matches(/^[a-zA-Z ]*$/, { message: 'lastName must be alphabets' })
+  country: string;
+
+  @ApiPropertyOptional({
+    description: 'cardName',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  // @Matches(/^[a-zA-Z ]*$/, { message: 'lastName must be alphabets' })
+  currency: string;
+}
